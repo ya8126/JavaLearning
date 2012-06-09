b16a4d9eb3414480d7d5465140ffb0a46f0b6f1f
ya8126 <yamamoto8836@hotmail.com>
2012-06-09 21:29:22 +0900

    Update master

--- src/jp/itacademy/samples/HelloWorld.java
+++ src/jp/itacademy/samples/HelloWorld.java
@@ -3,6 +3,6 @@ package jp.itacademy.samples;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World@‚ƒ‚‚‚‚‰‚”");
+		System.out.println("Hello World commit!!!");
 	}
 }

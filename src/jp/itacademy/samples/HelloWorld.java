c84750a3e31611502d4a47b22b90fe20ec379d29
itacademy <itacademy@192.168.1.9>
2012-06-09 20:41:05 +0900

    コミット2回目

--- src/jp/itacademy/samples/HelloWorld.java
+++ src/jp/itacademy/samples/HelloWorld.java
@@ -3,6 +3,6 @@ package jp.itacademy.samples;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World@‚ƒ‚‚‚‚‰‚”");
 	}
 }

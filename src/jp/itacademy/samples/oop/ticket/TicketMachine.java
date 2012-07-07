f4e3fd656aa383b11af9286593fe8e33ae0d34ef
itacademy <itacademy@192.168.1.9>
2012-07-07 21:32:32 +0900

    7/7

--- src/jp/itacademy/samples/oop/ticket/TicketMachine.java
+++ src/jp/itacademy/samples/oop/ticket/TicketMachine.java
@@ -1,5 +1,14 @@
 package jp.itacademy.samples.oop.ticket;
 
+/**
+ * 券売機を表すクラスです。お金を投入してチケットを購入することができます。
+ * 購入したチケットは<code>Ticket</code>クラスのオブジェクトとして
+ * 取得できます。
+ * 
+ * @author itacademy
+ * @version 1.0
+ *
+ */
 public class TicketMachine {
 	private static int nextSerial = 1;
 
@@ -11,16 +20,31 @@ public class TicketMachine {
 	private int price;
 	private int amount;
 
+	/**
+	 * この券売機で販売するチケットの名称と価格を指定して新しいインスタンスを生成します。
+	 * @param ticketName
+	 * @param price
+	 */
 	public TicketMachine(String ticketName, int price) {
 		super();
 		this.ticketName = ticketName;
 		this.price = price;
 	}
 
+	/**
+	 * お金を投入します。投入したお金は券売機の中に蓄積されます。
+	 * 
+	 * @param money　投入する金額
+	 */
 	public void putMoney(int money) {
 		this.amount += money;
 	}
 
+	/**
+	 * 現在券売機に入っているお金の合計を取得します
+	 * 
+	 * @return　券売機に入ってるお金
+	 */
 	public int getAmount() {
 		return amount;
 	}
